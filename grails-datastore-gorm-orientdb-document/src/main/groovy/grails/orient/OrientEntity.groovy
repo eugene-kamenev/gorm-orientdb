7e10bf29157f06458b9afba57d77ae6ba444d155
ekamenev <eugene.kamenev@gmail.com>
2016-06-29 15:59:13 +0600

    added entity persister

---
+++ grails-datastore-gorm-orientdb-document/src/main/groovy/grails/orient/OrientEntity.groovy
@@ -0,0 +1,12 @@
+package grails.orient
+
+import org.grails.datastore.gorm.GormEntity
+
+/**
+ * GORM OrientEntity class
+ *
+ * @author eugene.kamenev
+ */
+trait OrientEntity <D> extends GormEntity<D> {
+
+}
